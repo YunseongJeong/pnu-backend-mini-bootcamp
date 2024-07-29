10d39d21d85ab93b1d5fb4d7eb51f0dfd1ee8d8a
mandu <jys0972@gmail.com>
2024-07-29 13:39:01 +0900

    init spring boot

---
+++ src/main/java/com/ll/pnu_backend_mini_bootcamp/PnuBackendMiniBootcampApplication.java
@@ -0,0 +1,13 @@
+package com.ll.pnu_backend_mini_bootcamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PnuBackendMiniBootcampApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PnuBackendMiniBootcampApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ll/pnu_backend_mini_bootcamp/PnuBackendMiniBootcampApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.pnu_backend_mini_bootcamp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PnuBackendMiniBootcampApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
