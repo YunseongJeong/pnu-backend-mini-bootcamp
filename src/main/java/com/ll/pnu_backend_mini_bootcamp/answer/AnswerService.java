6944fb2040449f63533e740cfd9b0ee05b17bc55
mandu <jys0972@gmail.com>
2024-07-31 10:36:24 +0900

    feat(AnswerService.java) : Answer Service 구현

---
+++ src/main/java/com/ll/pnu_backend_mini_bootcamp/answer/AnswerService.java
@@ -0,0 +1,22 @@
+package com.ll.pnu_backend_mini_bootcamp.answer;
+
+import com.ll.pnu_backend_mini_bootcamp.question.Question;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@RequiredArgsConstructor
+@Service
+public class AnswerService {
+    private final AnswerRepository answerRepository;
+
+    public void create(Question question, String content){
+        Answer answer = new Answer();
+        answer.setContent(content);
+        answer.setCreateDate(LocalDateTime.now());
+        answer.setQuestion(question);
+        answerRepository.save(answer);
+    }
+
+}
