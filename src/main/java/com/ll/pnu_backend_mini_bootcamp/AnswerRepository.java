30eaaa9e5fbefff87816fd7ea280dfb92fb16bb6
mandu <jys0972@gmail.com>
2024-07-31 10:30:57 +0900

    feat(AnswerRepository.java) : Answer CRUD 구현

--- src/main/java/com/ll/pnu_backend_mini_bootcamp/AnswerRepository.java
+++
@@ -1,6 +0,0 @@
-package com.ll.pnu_backend_mini_bootcamp;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AnswerRepository extends JpaRepository<Answer, Integer> {
-}
