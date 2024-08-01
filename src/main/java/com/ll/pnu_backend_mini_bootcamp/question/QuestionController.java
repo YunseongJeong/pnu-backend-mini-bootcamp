551ee1c3a819949d29e4143acf25f15dd09c1cdc
mandu <jys0972@gmail.com>
2024-08-01 18:27:20 +0900

    feat(QuestionController.java) : 질문 column 추가로 인한 생성 로직 수정, 수정, 삭제, 추천 구현

--- src/main/java/com/ll/pnu_backend_mini_bootcamp/question/QuestionController.java
+++ src/main/java/com/ll/pnu_backend_mini_bootcamp/question/QuestionController.java
@@ -1,15 +1,21 @@
 package com.ll.pnu_backend_mini_bootcamp.question;
 
+import com.ll.pnu_backend_mini_bootcamp.User.SiteUser;
+import com.ll.pnu_backend_mini_bootcamp.User.SiteUserService;
 import com.ll.pnu_backend_mini_bootcamp.answer.AnswerForm;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
+import java.security.Principal;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -18,6 +24,7 @@ import java.util.List;
 public class QuestionController {
 
     private final QuestionService questionService;
+    private final SiteUserService siteUserService;
 
     @GetMapping("/list")
     public String list(Model model, @RequestParam(value="page", defaultValue="0") int page){
@@ -33,17 +40,68 @@ public class QuestionController {
         return "question_detail";
     }
 
+    @PreAuthorize("isAuthenticated()")
     @GetMapping("/create")
     public String create(QuestionForm questionForm){
         return "question_form";
     }
 
+    @PreAuthorize("isAuthenticated()")
     @PostMapping("/create")
-    public String create(@Valid QuestionForm questionForm, BindingResult bindingResult){
+    public String create(@Valid QuestionForm questionForm, BindingResult bindingResult, Principal principal){
         if (bindingResult.hasErrors()){
             return "question_form";
         }
-        questionService.create(questionForm.getSubject(), questionForm.getContent());
+        String username = principal.getName();
+        SiteUser author = siteUserService.getUser(username);
+        questionService.create(questionForm.getSubject(), questionForm.getContent(), author);
         return "redirect:list";
+    }
+
+    @GetMapping("modify/{id}")
+    @PreAuthorize("isAuthenticated()")
+    public String modify(QuestionForm questionForm, @PathVariable(value = "id") Integer id, Principal principal){
+        Question question = questionService.getQuestion(id);
+        if (!question.getAuthor().getUsername().equals(principal.getName())){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "수정권한이 없습니다.");
+        }
+        questionForm.setSubject(question.getSubject());
+        questionForm.setContent(question.getContent());
+        return "question_form";
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @PostMapping("/modify/{id}")
+    public String questionModify(@Valid QuestionForm questionForm, BindingResult bindingResult,
+                                 Principal principal, @PathVariable("id") Integer id) {
+        if (bindingResult.hasErrors()) {
+            return "question_form";
+        }
+        Question question = this.questionService.getQuestion(id);
+        if (!question.getAuthor().getUsername().equals(principal.getName())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "수정권한이 없습니다.");
+        }
+        this.questionService.modify(question, questionForm.getSubject(), questionForm.getContent());
+        return String.format("redirect:/question/detail/%s", id);
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/delete/{id}")
+    public String delete(@PathVariable(value = "id") Integer id, Principal principal){
+        Question question = questionService.getQuestion(id);
+        if (!question.getAuthor().getUsername().equals(principal.getName())){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "삭제권한이 없습니다.");
+        }
+        this.questionService.delete(question);
+        return "redirect:/";
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/vote/{id}")
+    public String vote(@PathVariable(value = "id") Integer id, Principal principal){
+        Question question = questionService.getQuestion(id);
+        SiteUser siteUser = siteUserService.getUser(principal.getName());
+        questionService.vote(question, siteUser);
+        return String.format("redirect:/question/detail/%s", id);
     }
 }
