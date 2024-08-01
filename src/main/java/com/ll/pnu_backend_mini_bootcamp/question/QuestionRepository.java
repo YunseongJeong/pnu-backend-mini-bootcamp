8c60d8cb009a5e6c2c08017ce4647842929b67ba
mandu <jys0972@gmail.com>
2024-08-01 18:28:22 +0900

    feat(QuestionRepository.java) : page 단위 fillAll 구현

--- src/main/java/com/ll/pnu_backend_mini_bootcamp/question/QuestionRepository.java
+++ src/main/java/com/ll/pnu_backend_mini_bootcamp/question/QuestionRepository.java
@@ -1,6 +1,9 @@
 package com.ll.pnu_backend_mini_bootcamp.question;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface QuestionRepository extends JpaRepository<Question, Integer> {
+    Page<Question> findAll(Pageable pagable);
 }
