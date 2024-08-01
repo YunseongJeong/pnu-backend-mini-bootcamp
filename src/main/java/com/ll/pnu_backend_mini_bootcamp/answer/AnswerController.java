0faeb661a4e96207de3f2e1d707d72940fdf2305
mandu <jys0972@gmail.com>
2024-08-01 17:43:18 +0900

    feat(AnswerController.java) : 답변 수정, 삭제, 추천, 앵커 구현

--- src/main/java/com/ll/pnu_backend_mini_bootcamp/answer/AnswerController.java
+++ src/main/java/com/ll/pnu_backend_mini_bootcamp/answer/AnswerController.java
@@ -1,13 +1,20 @@
 package com.ll.pnu_backend_mini_bootcamp.answer;
 
+import com.ll.pnu_backend_mini_bootcamp.User.SiteUser;
+import com.ll.pnu_backend_mini_bootcamp.User.SiteUserService;
 import com.ll.pnu_backend_mini_bootcamp.question.Question;
 import com.ll.pnu_backend_mini_bootcamp.question.QuestionService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.security.Principal;
 
 @RequiredArgsConstructor
 @RequestMapping("/answer")
@@ -16,15 +23,68 @@ public class AnswerController {
 
     private final QuestionService questionService;
     private final AnswerService answerService;
+    private final SiteUserService siteUserService;
 
+    @PreAuthorize("isAuthenticated()")
     @PostMapping("/create/{id}")
-    public String create(Model model, @PathVariable(value = "id") int id, @Valid AnswerForm answerForm, BindingResult bindingResult){
+    public String create(Model model, @PathVariable(value = "id") int id, @Valid AnswerForm answerForm, BindingResult bindingResult, Principal principal){
         Question question = questionService.getQuestion(id);
         if (bindingResult.hasErrors()){
             model.addAttribute("question", question);
             return "question_detail";
         }
-        answerService.create(question, answerForm.getContent());
-        return String.format("redirect:/question/detail/%s", id);
+        String username = principal.getName();
+        SiteUser author = siteUserService.getUser(username);
+        Answer answer =  answerService.create(question, answerForm.getContent(), author);
+        return String.format("redirect:/question/detail/%s#answer_%s",
+                answer.getQuestion().getId(), answer.getId());
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/modify/{id}")
+    public String modify(AnswerForm answerForm, @PathVariable(value = "id") Integer id, Principal principal){
+        Answer answer = answerService.getAnswer(id);
+        if (!answer.getAuthor().getUsername().equals(principal.getName())){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "수정 권한이 없습니다.");
+        }
+        answerForm.setContent(answer.getContent());
+        return "answer_form";
+    }
+    @PreAuthorize("isAuthenticated()")
+    @PostMapping("/modify/{id}")
+    public String answerModify(@Valid AnswerForm answerForm, BindingResult bindingResult,
+                               @PathVariable("id") Integer id, Principal principal) {
+        if (bindingResult.hasErrors()) {
+            return "answer_form";
+        }
+        Answer answer = this.answerService.getAnswer(id);
+        if (!answer.getAuthor().getUsername().equals(principal.getName())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "수정권한이 없습니다.");
+        }
+        this.answerService.modify(answer, answerForm.getContent());
+        return String.format("redirect:/question/detail/%s#answer_%s",
+                answer.getQuestion().getId(), answer.getId());
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/delete/{id}")
+    public String delete(@PathVariable(value = "id") Integer id, Principal principal){
+        Answer answer = answerService.getAnswer(id);
+        if (!answer.getAuthor().getUsername().equals(principal.getName())){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "삭제 권한이 없습니다.");
+        }
+        this.answerService.delete(answer);
+        return String.format("redirect:/question/detail/%s#answer_%s",
+                answer.getQuestion().getId(), answer.getId());
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/vote/{id}")
+    public String vote(@PathVariable(value = "id") Integer id, Principal principal){
+        Answer answer = answerService.getAnswer(id);
+        SiteUser siteUser = siteUserService.getUser(principal.getName());
+        answerService.vote(answer, siteUser);
+        return String.format("redirect:/question/detail/%s#answer_%s",
+                answer.getQuestion().getId(), answer.getId());
     }
 }
