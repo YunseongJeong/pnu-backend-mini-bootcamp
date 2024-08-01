e359107bfbaeda843a4cbc7c3e4dbd332d20bbcf
mandu <jys0972@gmail.com>
2024-08-01 18:34:11 +0900

    feat(UserCreateForm.java) : UserCreateForm 구현

---
+++ src/main/java/com/ll/pnu_backend_mini_bootcamp/User/UserCreateForm.java
@@ -0,0 +1,25 @@
+package com.ll.pnu_backend_mini_bootcamp.User;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserCreateForm {
+    @Size(min = 3, max = 25)
+    @NotEmpty(message = "username은 필수 항목입니다.")
+    private String username;
+
+    @NotEmpty(message = "password는 필수 항목입니다.")
+    private String password1;
+    @NotEmpty(message = "password 확인은 필수 항목입니다.")
+    private String password2;
+
+    @NotEmpty(message = "email은 필수 항목입니다.")
+    @Email
+    private String email;
+
+}
