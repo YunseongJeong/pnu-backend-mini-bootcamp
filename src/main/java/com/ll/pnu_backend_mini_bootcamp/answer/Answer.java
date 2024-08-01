2b84e7095f2dab1c88cdf23fa28f528318e735bd
mandu <jys0972@gmail.com>
2024-08-01 17:41:57 +0900

    feat(Answer.java) : 수정날짜, 유저, 추천인 columns 추가

--- src/main/java/com/ll/pnu_backend_mini_bootcamp/answer/Answer.java
+++ src/main/java/com/ll/pnu_backend_mini_bootcamp/answer/Answer.java
@@ -1,11 +1,13 @@
 package com.ll.pnu_backend_mini_bootcamp.answer;
 
+import com.ll.pnu_backend_mini_bootcamp.User.SiteUser;
 import com.ll.pnu_backend_mini_bootcamp.question.Question;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
+import java.util.Set;
 
 @Getter
 @Setter
@@ -20,6 +22,14 @@ public class Answer {
 
     private LocalDateTime createDate;
 
+    private LocalDateTime modifyDate;
+
     @ManyToOne
     private Question question;
+
+    @ManyToOne
+    private SiteUser author;
+
+    @ManyToMany
+    private Set<SiteUser> voter;
 }
