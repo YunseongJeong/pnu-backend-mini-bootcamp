ee853f1e2f8a7c27c3116324d75acaa3515da0d0
mandu <jys0972@gmail.com>
2024-08-01 18:34:35 +0900

    feat(UserRole.java) : UserRole enum 구현

---
+++ src/main/java/com/ll/pnu_backend_mini_bootcamp/User/UserRole.java
@@ -0,0 +1,15 @@
+package com.ll.pnu_backend_mini_bootcamp.User;
+
+import lombok.Getter;
+
+@Getter
+public enum UserRole {
+    ADMIN("ROLE_ADMIN"),
+    USER("ROLE_USER");
+
+    UserRole(String value) {
+        this.value = value;
+    }
+
+    private String value;
+}
