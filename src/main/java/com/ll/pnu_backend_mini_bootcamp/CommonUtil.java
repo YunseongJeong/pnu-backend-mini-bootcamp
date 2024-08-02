020ecd6e3d36cf35165218a16bf8a41b04bcf28e
mandu <jys0972@gmail.com>
2024-08-02 10:30:35 +0900

    feat(CommonUtil.java) : markdown 변환 Component 구현

---
+++ src/main/java/com/ll/pnu_backend_mini_bootcamp/CommonUtil.java
@@ -0,0 +1,16 @@
+package com.ll.pnu_backend_mini_bootcamp;
+
+import org.commonmark.node.Node;
+import org.commonmark.parser.Parser;
+import org.commonmark.renderer.html.HtmlRenderer;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CommonUtil {
+    public String markdown(String markdown) {
+        Parser parser = Parser.builder().build();
+        Node document = parser.parse(markdown);
+        HtmlRenderer htmlRenderer = HtmlRenderer.builder().build();
+        return htmlRenderer.render(document);
+    }
+}
