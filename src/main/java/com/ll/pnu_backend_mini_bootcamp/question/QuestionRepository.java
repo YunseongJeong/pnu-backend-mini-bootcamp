dcd1169a49b22b5939da23fff0f88b392871597c
mandu <jys0972@gmail.com>
2024-08-02 13:33:29 +0900

    feat(QuestionRepository.java) : Specification을 통한 findAll 추가

--- src/main/java/com/ll/pnu_backend_mini_bootcamp/question/QuestionRepository.java
+++ src/main/java/com/ll/pnu_backend_mini_bootcamp/question/QuestionRepository.java
@@ -2,8 +2,10 @@ package com.ll.pnu_backend_mini_bootcamp.question;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface QuestionRepository extends JpaRepository<Question, Integer> {
     Page<Question> findAll(Pageable pagable);
+    Page<Question> findAll(Specification<Question> spec, Pageable pageable);
 }
